af34d25f324f5cfee1dccad0d921c08b9ae7bec7
drunkpussy <418511740@qq.com>
2019-06-19 07:21:14 +0800

    hard code animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,52 @@
 import { setUpControls } from './controls.js'
-import { loadMaterial } from './loader.js'
+import { loadCubemap } from './loader.js'
 
 let scene, camera, renderer;
 let materialName = '1';
+let counter = 0;
+let fov, zoom = 1.0, inc = -0.007;
 
 function init() {
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 45, 30000);
-    camera.position.set(-900, -200, -900);
+    camera.position.set(-900, -100, -900);
+    fov = camera.fov
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     setUpControls(camera, scene);
-    let materialArray = loadMaterial(materialName);
+    let cubemap = loadCubemap(materialName)
 
-    let cubemapGeo = new THREE.BoxGeometry(10000, 10000, 10000);
-    let cubemap = new THREE.Mesh(cubemapGeo, materialArray);
     scene.add(cubemap);
     animate();
+
 }
 
 function animate() {
+    counter++;
+    // console.log(counter)
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
+
+    if (counter == 100) {
+        console.log(camera.position)
+    } else if (counter > 100 && counter < 150) {
+        camera.fov = fov * zoom;
+        camera.updateProjectionMatrix();
+        zoom += inc;
+    } else if (counter == 150) {
+        console.log(scene)
+        camera.position.set(-900, -100, -900);
+        // scene.remove(scene.children[0])
+        materialName = '2';
+        let cubemap = loadCubemap(materialName)
+        scene.add(cubemap);
+    }
+
+    
 }
 
 init();
