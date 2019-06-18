1ad051c55d780ad12d47c22c98fe6c644b4a048c
drunkpussy <418511740@qq.com>
2019-06-18 12:44:40 +0800

    orbit controls

---
+++ public/js/controls.js
@@ -0,0 +1,9 @@
+export function setUpControls(camera, renderer) {
+
+    let controls = new THREE.OrbitControls(camera);
+
+    controls.addEventListener('change', renderer);
+    controls.minDistance = 500;
+    controls.maxDistance = 1500;
+    
+}
