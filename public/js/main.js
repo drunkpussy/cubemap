64d4016e3acba4c0dde615f5020656b46a2ee58d
drunkpussy <418511740@qq.com>
2019-06-23 21:33:26 +0800

    main function

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,13 @@
 import { setUpControls } from './controls.js'
+import { setUpComposer } from './composer.js'
 import { loadCubemap } from './loader.js'
 
 let scene, camera, renderer;
 let materialName = '1';
 let counter = 0;
-let fov, zoom = 1.0, inc = -0.007;
+// let fov, zoom = 1.0, inc = -0.007;
+let composer;
+let cubemap;
 
 function init() {
     scene = new THREE.Scene();
@@ -17,32 +20,36 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    
+    composer = setUpComposer(renderer, scene, camera)
     setUpControls(camera, scene);
-    let cubemap = loadCubemap(materialName)
+    cubemap = loadCubemap(materialName)
 
     scene.add(cubemap);
     animate();
 
 }
 
-function animate() {
-    counter++;
+function animate(currentCube) {
+    counter += 2;
     // console.log(counter)
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 
     if (counter == 100) {
-        console.log(camera.position)
-    } else if (counter > 100 && counter < 150) {
-        camera.fov = fov * zoom;
-        camera.updateProjectionMatrix();
-        zoom += inc;
-    } else if (counter == 150) {
-        console.log(scene)
+        // console.log(camera.position)
+    } else if (counter > 100 && counter < 300) {
+        // camera.fov = fov * zoom;
+        // camera.updateProjectionMatrix();
+        // zoom += inc;
+        composer.render();
+        cubemap.position.z -= 30;
+    } else if (counter == 300) {
+        // console.log(scene)
         camera.position.set(-900, -100, -900);
-        // scene.remove(scene.children[0])
+        scene.remove(scene.children[0])
         materialName = '2';
-        let cubemap = loadCubemap(materialName)
+        cubemap = loadCubemap(materialName)
         scene.add(cubemap);
     }
 
